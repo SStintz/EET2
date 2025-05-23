8c1451bdb42d45aa5364cc19dff45b10864c5e36
S Stintz <sofiastintztorrejon@gmail.com>
2025-05-23 19:30:26 +0200

    Revert "5.2 Testen der Tastereingaben"
    This reverts commit fd230ad1bc58c44ca99d82c21cdf25f52f04cee7.

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,15 @@
 #include <Arduino.h>
 
-//Pinbelegung aus Versuchsstand
-
-const int taster1 = 0;
-const int taster2 = 1;
-const int led1 = 14; 
-
 void setup() {
   // initialisiert Pin 14 (LED1) als output
-  pinMode(taster1, INPUT);
-  pinMode(taster2, INPUT);
-  pinMode(led1, OUTPUT);
+  pinMode(14, OUTPUT);
 }
 //LED 1 blinkt im Zweisekundentakt
 
 void loop() {
-  if (digitalRead(taster1) == HIGH) {
-    digitalWrite(led1, HIGH);
-  }
-  if (digitalRead(taster2) == HIGH) {
-    digitalWrite(led1, LOW);
-  }
-  //digitalWrite(14, HIGH);
-  //delay(1000);
-  //digitalWrite(14, LOW);
-  //delay(1000);
+  digitalWrite(14, HIGH);
+  delay(1000);
+  digitalWrite(14, LOW);
+  delay(1000);
 }
 
