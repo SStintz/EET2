5496b29295a590200806780f1196a15bf48932f6
S Stintz <sofiastintztorrejon@gmail.com>
2025-05-24 08:14:15 +0200

    5.4 Einstellen eines festen Zundwinkels

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,41 @@
 #include <Arduino.h>
 
 //Pinbelegung aus Versuchsstand
-int nullDurchgang = 0;
+//int nullDurchgang = 0;
 const int triacPin = 3;
 const int interruptPin = 9;
-
-void zaehleNullDurchgang() {
-    nullDurchgang++;
-}
+bool zerocrossFlag = false;
+const unsigned long delayAfterZeroCross = 8000;
 
 void setup() {
   // initialisiert Pin 14 (LED1) als output
   pinMode(triacPin, OUTPUT);
+  digitalWrite(triacPin, LOW);
   pinMode(interruptPin, INPUT);
 
   // Interrupt bei steigendem Signal (= Nulldurchgang)
-  attachInterrupt(digitalPinToInterrupt(interruptPin), zaehleNullDurchgang, RISING);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), onZeroCross, RISING);
 }
 
 void loop() {
-  if (nullDurchgang >= 10) {
-    digitalWrite(triacPin, HIGH);
-    delay(10); // Nur kurz einschalten
-    digitalWrite(triacPin, LOW);
+   if (zerocrossFlag) { //if es gab Nulldurchgang, züruck auf False setzen
+    zerocrossFlag = false;
 
-    nullDurchgang = 0; // Zähler zurücksetzen
+    delayMicroseconds(delayAfterZeroCross);  // Phasenanschnitt
+
+    digitalWrite(triacPin, HIGH);            // Triac zünden
+    delayMicroseconds(10);                    // kurze Pulsdauer
+    digitalWrite(triacPin, LOW);               //Triac bleibt leitend, solange wieder Nulldurchgang
   }
  
 }
  
+void onZeroCross() {
+  zerocrossFlag = true;   // Nulldurchgang registrieren
+}
+
+
+
+
 
 
